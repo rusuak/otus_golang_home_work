f7cd4054b26e92658284a2a854799f03e6497ccb
RusuAK <aleksejj.rusu@vseinstrumenti.ru>
2022-08-31 22:04:18 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,28 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	middleChannel := make(Bi)
+	go func() {
+		defer close(middleChannel)
+		for {
+			select {
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				middleChannel <- v
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	nextStage, stages := stages[0], stages[1:]
+	out := nextStage(middleChannel)
+
+	if len(stages) > 0 {
+		return ExecutePipeline(out, done, stages...)
+	}
+
+	return out
 }
