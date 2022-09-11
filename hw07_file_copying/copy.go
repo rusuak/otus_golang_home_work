5ecf157a0d73d9bba7de63248189539e15824d17
RusuAK <aleksejj.rusu@vseinstrumenti.ru>
2022-09-11 16:44:27 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,89 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb"
+)
+
+const (
+	copyChunkSize int64 = 100
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrOffsetLimitNegative   = errors.New("offset and limit must not be negative")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if offset < 0 || limit < 0 {
+		return ErrOffsetLimitNegative
+	}
+
+	sourceFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destFile, err := os.OpenFile(toPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0777)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	fileSize, err := getFileSize(sourceFile)
+	if err != nil {
+		return err
+	}
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+	bytesToCopyCount := fileSize - offset
+	if limit == 0 || limit > bytesToCopyCount {
+		limit = bytesToCopyCount
+	}
+
+	err = doCopy(sourceFile, destFile, offset, limit)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func doCopy(sourceFile *os.File, destFile *os.File, offset int64, limit int64) error {
+	sourceFile.Seek(offset, 0)
+	sourceReader := io.LimitReader(sourceFile, limit)
+
+	bar := pb.StartNew(int(limit))
+	for {
+		n, err := io.CopyN(destFile, sourceReader, copyChunkSize)
+		bar.Add64(n)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return err
+		}
+	}
+	bar.Finish()
+
+	return nil
+}
+
+func getFileSize(sourceFile *os.File) (int64, error) {
+	fileStat, err := sourceFile.Stat()
+	if err != nil {
+		return 0, err
+	}
+
+	fileSize := fileStat.Size()
+	if fileSize <= 0 {
+		return 0, ErrUnsupportedFile
+	}
+
+	return fileSize, nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,61 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("unsupported file error case", func(t *testing.T) {
+		from := "/dev/urandom"
+		to := "copy_test.txt"
+		limit := int64(0)
+		offset := int64(0)
+
+		err := Copy(from, to, offset, limit)
+		require.Truef(t, errors.Is(err, ErrUnsupportedFile), "actual err - %v", err)
+
+		os.Remove(to)
+	})
+
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		from := "test_offset_exceeds_file_size_source"
+		to := "test_offset_exceeds_file_size_dest"
+		limit := int64(0)
+		offset := int64(4)
+
+		fileFrom, _ := os.Create(from)
+		fileFrom.WriteString("123")
+		os.Create(to)
+
+		err := Copy(from, to, offset, limit)
+		require.Truef(t, errors.Is(err, ErrOffsetExceedsFileSize), "actual err - %v", err)
+
+		os.Remove(from)
+		os.Remove(to)
+	})
+
+	t.Run("valid case", func(t *testing.T) {
+		from := "test_offset_exceeds_file_size_source"
+		to := "test_offset_exceeds_file_size_dest"
+		limit := int64(4)
+		offset := int64(5)
+
+		fileFrom, _ := os.Create(from)
+		fileFrom.WriteString("some content")
+		fileTo, _ := os.Create(to)
+
+		err := Copy(from, to, offset, limit)
+		require.Equal(t, nil, err)
+
+		destContent := make([]byte, 4)
+		fileTo.Read(destContent)
+		require.Equal(t, "cont", string(destContent))
+
+		os.Remove(from)
+		os.Remove(to)
+	})
 }
