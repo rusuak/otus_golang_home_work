14cd176a993e8943ba013629958ae163f9dcf3fb
RusuAK <aleksejj.rusu@vseinstrumenti.ru>
2022-09-18 16:24:32 +0300

    HW8 is completed

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	args := os.Args
+	if len(args) < 3 {
+		os.Stdout.WriteString("Please pass the directory path and the command to execute\n")
+
+		return
+	}
+
+	dir, err := ReadDir(args[1])
+	if err != nil {
+		os.Stdout.WriteString(fmt.Sprintln(err))
+
+		return
+	}
+
+	RunCmd(args[2:], dir)
 }
